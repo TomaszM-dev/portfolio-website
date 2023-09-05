f69c34c42bd874ecd748f7ad770ceb13d1eb945b
TomaszM-dev <tomekmalocha@gmail.com>
2023-09-05 17:01:28 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Hydrate from "./components/Hydrate";
+import Head from "@/node_modules/next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,19 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-        />
-        <body className={inter.className}>
-          <div className="container ">
-            <Nav />
-            {children}
-            <Footer />
-          </div>
-        </body>
-      </head>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body className={inter.className}>
+        <div className="container ">
+          <Nav />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
